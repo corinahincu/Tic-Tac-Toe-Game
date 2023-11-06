bb89f1d342d661452566f3815dbfe71fe05bd3cc
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-11-06 23:25:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -121,7 +121,7 @@ class Game extends React.Component {
 }
 
 
-// ========================================
+// -------------------------------------------------------------------------
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
